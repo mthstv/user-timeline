9ee1211775ccb3f4f34e9fe5d8dfa16b4adc6f46
Matheus Tavares <matheus.tavares45@gmail.com>
2025-03-01 15:05:28 -0300

    feat: move user entity to entities folder on auth service

--- packages/microservices-nest/auth-service/src/database/migrations/1740793897854-migration.ts
+++ packages/microservices-nest/auth-service/src/database/migrations/1740793897854-migration.ts
@@ -1,14 +1,17 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Migration1740793897854 implements MigrationInterface {
-    name = 'Migration1740793897854'
+  name = 'Migration1740793897854';
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "name" DROP NOT NULL`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ALTER COLUMN "name" DROP NOT NULL`,
+    );
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" ALTER COLUMN "name" SET NOT NULL`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(
+      `ALTER TABLE "user" ALTER COLUMN "name" SET NOT NULL`,
+    );
+  }
 }
