9421d41e0e71f54597aea1d17463fbcad90178f5
Matheus Tavares <matheus.tavares45@gmail.com>
2025-03-01 20:53:14 -0300

    feat: installing dropdown menu and buttons and creating side menu

---
+++ packages/frontend-nextjs/app/(layout)/feed/page.tsx
@@ -0,0 +1,9 @@
+import { ThemeToggle } from "@/components/shared/theme-toggle";
+
+export default function Feed() {
+  return (
+    <main className="h-full w-full">
+      <h1>Feed</h1>
+    </main>
+  );
+}
--- packages/frontend-nextjs/app/layout.tsx
+++ packages/frontend-nextjs/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/shared/theme-provider";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,8 +17,21 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="pt-BR" suppressHydrationWarning>
+      <body
+        className={cn(
+          'min-h-screen bg-background',
+          inter.className
+        )}>
+           <ThemeProvider
+            attribute="class"
+            defaultTheme="system"
+            enableSystem
+            disableTransitionOnChange
+          >
+            {children}
+          </ThemeProvider>
+        </body>
     </html>
   );
 }
