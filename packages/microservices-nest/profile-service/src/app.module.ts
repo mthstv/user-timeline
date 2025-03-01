21ae6d64d9c56db87f660c25274fe26cb2d0a981
Matheus Tavares <matheus.tavares45@gmail.com>
2025-03-01 17:27:44 -0300

    feat: add auth flow to profile service

--- packages/microservices-nest/profile-service/src/app.module.ts
+++ packages/microservices-nest/profile-service/src/app.module.ts
@@ -1,11 +1,24 @@
+import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AuthModule } from './auth/auth.module';
 import { ProfilesModule } from './profiles/profiles.module';
 
 @Module({
-  imports: [ProfilesModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    AuthModule,
+    ProfilesModule,
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DATABASE_HOST,
+      port: parseInt(process.env.DATABASE_PORT || '', 5432),
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE_NAME || 'profile-service',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'],
+      synchronize: true,
+    }),
+  ],
 })
 export class AppModule {}
