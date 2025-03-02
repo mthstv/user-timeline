de7d4fa7d07ca71093fe9e76805b493b25610735
Matheus Tavares <matheus.tavares45@gmail.com>
2025-03-02 14:03:47 -0300

    feat: finish up edit dialog layout

--- packages/frontend-nextjs/components/pages/profile/edit-profile-dialog.tsx
+++ packages/frontend-nextjs/components/pages/profile/edit-profile-dialog.tsx
@@ -1,12 +1,18 @@
 import { Button } from '@/components/ui/button';
 import { Dialog } from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Textarea } from '@/components/ui/textarea';
+import { AtSign } from 'lucide-react';
+import Image from 'next/image';
 
 type EditProfileDialogProps = {
+  user: Record<string, string | null>;
   open: boolean;
   setOpen: (open: boolean) => void;
 };
 
 export const EditProfileDialog = ({
+  user,
   open,
   setOpen,
 }: EditProfileDialogProps) => {
@@ -17,9 +23,78 @@ export const EditProfileDialog = ({
       title="Edit profile"
       description="Change your profile as you like!"
       content={
-        <div className="flex flex-col gap-4">
-          <Button>Salvar</Button>
-        </div>
+        <main className="flex flex-col gap-4">
+          <div className="flex gap-4">
+            <div className="flex flex-col gap-4 justify-start items-center">
+              <div className="h-28 w-28 rounded-full border border-muted-foreground flex items-center justify-center bg-secondary z-10">
+                {user.avatar ? (
+                  <Image
+                    alt="user profile image"
+                    className="rounded-full z-0"
+                    src={user.avatar}
+                    width={112}
+                    height={112}
+                  />
+                ) : (
+                  <p className="text-3xl text-muted-foreground">JD</p>
+                )}
+              </div>
+            </div>
+            <div className="flex flex-col gap-4 w-full">
+              <div className="flex flex-col gap-2">
+                <label
+                  htmlFor="avatar"
+                  className="block text-sm text-muted-foreground"
+                >
+                  Avatar
+                </label>
+                <Input
+                  placeholder="https://..."
+                  id="avatar"
+                  value={user.avatar || ''}
+                />
+              </div>
+              <div className="flex flex-col gap-2">
+                <label
+                  htmlFor="name"
+                  className="block text-sm text-muted-foreground"
+                >
+                  Name
+                </label>
+                <Input id="name" value={user.name || ''} />
+              </div>
+
+              <div className="flex flex-col gap-2">
+                <label
+                  htmlFor="username"
+                  className="block text-sm text-muted-foreground"
+                >
+                  Username
+                </label>
+                <div className="flex items-center">
+                  <span className="bg-secondary border border-secondary rounded p-[5px] rounded-r-none">
+                    <AtSign />
+                  </span>
+                  <Input
+                    className="rounded-l-none"
+                    id="username"
+                    value={user.username || ''}
+                  />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="flex flex-col gap-2">
+            <label
+              htmlFor="bio"
+              className="block text-sm text-muted-foreground"
+            >
+              Bio
+            </label>
+            <Textarea id="bio" value={user.bio || ''} />
+          </div>
+          <Button onClick={() => setOpen(false)}>Salvar</Button>
+        </main>
       }
     />
   );
