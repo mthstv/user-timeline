43a1e345f4ba2b9b26d36f4cc50f111e94362bab
Matheus Tavares <matheus.tavares45@gmail.com>
2025-03-02 12:02:37 -0300

    hotfix: avatar size adjustments

--- packages/frontend-nextjs/components/shared/profile-tooltip.tsx
+++ packages/frontend-nextjs/components/shared/profile-tooltip.tsx
@@ -25,7 +25,7 @@ export function ProfileTooltip({ children }: ProfileTooltipProps) {
         <TooltipTrigger asChild>{children}</TooltipTrigger>
         <TooltipContent className="bg-background border border-muted text-muted-foreground flex flex-col gap-4 m-2 max-w-[200px]">
           <div className="flex items-center gap-4">
-            <Avatar user={userProfile} size="10" />
+            <Avatar user={userProfile} />
             <div className="flex flex-col">
               <span className="font-semibold text-white">
                 {userProfile.name}
