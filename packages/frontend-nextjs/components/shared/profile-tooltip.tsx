4a08601658203ee1af114857d650ee01c232adb0
Matheus Tavares <matheus.tavares45@gmail.com>
2025-03-02 10:33:27 -0300

    hotfix: change max width of profile tooltip

--- packages/frontend-nextjs/components/shared/profile-tooltip.tsx
+++ packages/frontend-nextjs/components/shared/profile-tooltip.tsx
@@ -23,7 +23,7 @@ export function ProfileTooltip({ children }: ProfileTooltipProps) {
     <TooltipProvider>
       <Tooltip delayDuration={300}>
         <TooltipTrigger asChild>{children}</TooltipTrigger>
-        <TooltipContent className="bg-background border border-muted text-muted-foreground flex flex-col gap-4 m-2 max-w-[180px]">
+        <TooltipContent className="bg-background border border-muted text-muted-foreground flex flex-col gap-4 m-2 max-w-[200px]">
           <div className="flex items-center gap-4">
             <Avatar user={userProfile} size="10" />
             <div className="flex flex-col">
