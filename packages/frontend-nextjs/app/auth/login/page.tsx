3957ea39b4aef43f56a487eda689eabc716afffd
Matheus Tavares <matheus.tavares45@gmail.com>
2025-03-03 00:37:32 -0300

    feat: adds login form and integration

--- packages/frontend-nextjs/app/auth/login/page.tsx
+++ packages/frontend-nextjs/app/auth/login/page.tsx
@@ -1,9 +1,7 @@
 'use client';
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
-import { Input } from '@/components/ui/input';
-import { Button } from '@/components/ui/button';
-import Link from 'next/link';
+import { LoginForm } from '@/components/pages/login/login-form';
 
 export default function Login() {
   return (
@@ -13,48 +11,7 @@ export default function Login() {
           <CardTitle className="text-2xl text-center">Login</CardTitle>
         </CardHeader>
         <CardContent>
-          <form className="space-y-4">
-            <div className="flex flex-col gap-1">
-              <label
-                htmlFor="email"
-                className="block text-sm text-muted-foreground"
-              >
-                Email
-              </label>
-              <Input
-                id="email"
-                type="email"
-                placeholder="you@example.com"
-                required
-              />
-            </div>
-            <div className="flex flex-col gap-1">
-              <label
-                htmlFor="password"
-                className="block text-sm text-muted-foreground"
-              >
-                Password
-              </label>
-              <Input
-                id="password"
-                type="password"
-                placeholder="********"
-                required
-              />
-            </div>
-            <Button type="submit" className="w-full my-4">
-              Sign In
-            </Button>
-            <Link href="/auth/signup">
-              <Button
-                type="button"
-                className="w-full text-sm flex items-center justify-center text-center"
-                variant="link"
-              >
-                {"Don't have an account? Sign up here"}
-              </Button>
-            </Link>
-          </form>
+          <LoginForm />
         </CardContent>
       </Card>
     </div>
