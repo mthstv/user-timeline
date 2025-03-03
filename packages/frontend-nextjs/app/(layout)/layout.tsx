23c7026ce0aa321b00112575dfbf5ce05512ae7e
Matheus Tavares <matheus.tavares45@gmail.com>
2025-03-03 00:38:35 -0300

    feat: show user display name on user dropdown

--- packages/frontend-nextjs/app/(layout)/layout.tsx
+++ packages/frontend-nextjs/app/(layout)/layout.tsx
@@ -5,8 +5,10 @@ import { UserDropdown } from '@/components/pages/sidebar/user-dropdown';
 import { ThemeToggle } from '@/components/shared/theme-toggle';
 import { Button } from '@/components/ui/button';
 import { cn } from '@/lib/utils';
+import { getUserSession } from '@/services/auth';
 import { Menu } from 'lucide-react';
-import { useState } from 'react';
+import { Session } from 'next-auth';
+import { useEffect, useState } from 'react';
 
 type DashboardLayoutProps = {
   children: React.ReactNode;
@@ -14,7 +16,16 @@ type DashboardLayoutProps = {
 
 export default function DashboardLayout({ children }: DashboardLayoutProps) {
   const [sidebarOpen, setSidebarOpen] = useState(true);
+  const [session, setSession] = useState<Session | null>(null);
 
+  useEffect(() => {
+    const fetchSession = async () => {
+      const authSession = await getUserSession();
+      setSession(authSession);
+    };
+
+    fetchSession();
+  }, []);
 
   return (
     <div className="w-full h-screen overflow-hidden lg:grid lg:grid-cols-[300px,1fr] relative">
@@ -27,25 +38,27 @@ export default function DashboardLayout({ children }: DashboardLayoutProps) {
         <Menu />
       </Button>
 
-      <aside className={cn(
-        "w-[300px] h-full flex flex-col items-center border-r border-muted bg-background",
-        "fixed lg:relative transition-transform duration-300 z-40",
-        !sidebarOpen && "-translate-x-full lg:translate-x-0"
-      )}>
+      <aside
+        className={cn(
+          'w-[300px] h-full flex flex-col items-center border-r border-muted bg-background',
+          'fixed lg:relative transition-transform duration-300 z-40',
+          !sidebarOpen && '-translate-x-full lg:translate-x-0'
+        )}
+      >
         <div className="w-full p-8 border-b border-muted">
           <div className="max-w-[100px] mx-auto"></div>
         </div>
         <NavItems />
         <div className="w-full mt-auto border-t border-muted px-3 py-4 flex items-center justify-between gap-2">
-          <UserDropdown user={{ name: 'John Doe' }} />
+          <UserDropdown user={session?.user} />
           <ThemeToggle />
         </div>
       </aside>
 
-      <div 
+      <div
         className={cn(
-          "fixed inset-0 bg-black/50 lg:hidden transition-opacity duration-300",
-          sidebarOpen ? "opacity-100" : "opacity-0 pointer-events-none"
+          'fixed inset-0 bg-black/50 lg:hidden transition-opacity duration-300',
+          sidebarOpen ? 'opacity-100' : 'opacity-0 pointer-events-none'
         )}
         onClick={() => setSidebarOpen(false)}
       />
