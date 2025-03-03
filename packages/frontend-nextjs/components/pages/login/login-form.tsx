17791575a71ab1d6cebd99214d338b3eb5a98e4f
Matheus Tavares <matheus.tavares45@gmail.com>
2025-03-03 00:37:32 -0300

    feat: adds login form and integration

---
+++ packages/frontend-nextjs/components/pages/login/login-form.tsx
@@ -0,0 +1,64 @@
+'use client';
+
+import { Input } from '@/components/ui/input';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { useForm } from 'react-hook-form';
+import { useMutation } from '@tanstack/react-query';
+import { handleSignIn } from '@/services/auth';
+
+export const LoginForm = () => {
+  const { register, handleSubmit } = useForm();
+
+  const { mutate: handleLogin } = useMutation({
+    mutationFn: handleSignIn,
+  });
+
+  const onSubmit = (data: SigninDto) => {
+    handleLogin(data);
+  };
+
+  return (
+    <form className="space-y-4" onSubmit={handleSubmit(onSubmit)}>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="email" className="block text-sm text-muted-foreground">
+          Email
+        </label>
+        <Input
+          id="email"
+          type="email"
+          placeholder="you@example.com"
+          required
+          {...register('email', { required: true })}
+        />
+      </div>
+      <div className="flex flex-col gap-1">
+        <label
+          htmlFor="password"
+          className="block text-sm text-muted-foreground"
+        >
+          Password
+        </label>
+        <Input
+          id="password"
+          type="password"
+          placeholder="********"
+          required
+          {...register('password', { required: true })}
+        />
+      </div>
+      <Button type="submit" className="w-full my-4">
+        Sign In
+      </Button>
+      <Link href="/auth/signup">
+        <Button
+          type="button"
+          className="w-full text-sm flex items-center justify-center text-center"
+          variant="link"
+        >
+          {"Don't have an account? Sign up here"}
+        </Button>
+      </Link>
+    </form>
+  );
+};
