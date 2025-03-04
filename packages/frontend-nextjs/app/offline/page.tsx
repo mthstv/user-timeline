739f8eda7e44e122b4195ea0961be0f2c2827e70
Matheus Tavares <matheus.tavares45@gmail.com>
2025-03-04 13:31:21 -0300

    feat: add next-pwa for offline functionality

---
+++ packages/frontend-nextjs/app/offline/page.tsx
@@ -0,0 +1,8 @@
+export default function Offline() {
+  return (
+    <div className="p-8 text-center">
+      <h1>🔌 You are offline</h1>
+      <p>{"Sorry, you don't have an internet connection."}</p>
+    </div>
+  );
+}
